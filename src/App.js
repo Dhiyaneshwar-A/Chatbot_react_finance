1d7787ca076c3d342d5be92e1096ce8e7c1fa877
Dhiyaneshwar-A <dhiyanesh0704@gmail.com>
2024-09-03 15:31:17 +0530

    feat:finance chatbot

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Chatbot from './components/Chatbot';
+import './styles.css'; // Import your styles
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="legaldad.png" alt="LegalAppa Logo" className="logo" />
+      </header>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
